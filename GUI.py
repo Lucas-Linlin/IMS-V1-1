039c152c6f9c6ce19ca668305a5d9f522546ac9d
lin-lucas <zhilin.tang@qq.com>
2025-01-17 14:39:58 +0800

    feat(ui): 添加登录功能和用户物品信息

--- GUI.py
+++ GUI.py
@@ -5,6 +5,23 @@ def _add(win:Tk,directory, name):
     add(directory, name)
     win.destroy()
 
+def _login(win:Tk,username, password):
+    login(username, password)
+    win.destroy()
+
+def GUI_login():
+    window = Tk()
+    window.title("登录")
+    window.geometry("200x120")
+    Label(window, text="用户名：").pack()
+    username = Entry(window)
+    username.pack()
+    Label(window, text="密码：").pack()
+    password = Entry(window, show="*")
+    password.pack()
+    Button(window, text="登录", width=10, command=lambda: _login(window,username.get(), password.get())).pack()
+    window.mainloop()
+
 def GUI_add():
     window = Tk()
     window.title("添加")
@@ -19,7 +36,7 @@ def GUI_add():
     window.mainloop()
 
 def main():
-    logister()
+    GUI_login()
     readInfo()
     root = Tk()
     root.title("仓储物品管理系统")
@@ -30,7 +47,8 @@ def main():
     menu.add_command(label='添加', command=GUI_add)
     root.config(menu=menu)
     # Display
-    Label(root, text="仓库")
+    Label(root, text="仓库").pack()
+    Text(root, height=20, width=50).pack()
     root.mainloop()
 
 if __name__ == '__main__':
