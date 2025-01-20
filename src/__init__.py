9ee861f6e9e09124c58c7aee3ee74279fd80e68b
lin-lucas <zhilin.tang@qq.com>
2025-01-20 21:54:44 +0800

    refactor: 更新构建脚本并改进日志和用户交互

--- src/__init__.py
+++ src/__init__.py
@@ -57,14 +57,14 @@ def add(index, value):
             things[index] = {value:1}
     FILE[user]["things"] = things
     with open(rootPath / f'users.json', 'w') as file:
-        print(f'{value} added to {index}。')
+        print(f'{value} 被添加到 {index}。')
     log('添加成功。')
 
 def delete(index, value):
     log(f'用户删除某物。')
     global things, user
     if index not in things.keys():
-        print(f'Index "{index}" does not exist.')
+        print(f'索引 "{index}" 不存在。')
         return
     try:
         things[index][value] -= 1
@@ -73,16 +73,16 @@ def delete(index, value):
         if len(things[index]) == 0:
             del things[index]
     except KeyError:
-        print(f'{value} is not found in {index}.')
+        print(f'{value} 不在 {index} 中。')
         return
     FILE[user]["things"] = things
     with open(rootPath / f'users.json', 'w') as file:
         json.dump(FILE, file, indent=4, sort_keys=True)
-    print(f'{value} deleted from {index}.')
+    print(f'{value} 被从 {index} 中删除。')
     log('删除成功。')
 
 def search(value):
-    log(f'用户搜索某物。')
+    log(f'用户查询某物。')
     global things
     found = {} # {address: count, ...}
     returnStr = ''
@@ -90,15 +90,15 @@ def search(value):
         for j in things[i].keys():
             if value.lower() in j.lower():
                 found[i] = things[i][j]
-                returnStr += f'At {i}: {things[i][j]}*{j}\n'
+                returnStr += f'{i}: {things[i][j]}*{j}\n'
     if returnStr == '':
         log('无结果。')
-        print('No results.')
+        print('无结果。')
         return 0,''
     else:
-        print(f'Found:')
+        print(f'查询成功：')
         print(returnStr[:-1])
-        log('搜索成功。')
+        log('查询成功。')
     return len(found), returnStr
 
 def query(index):
@@ -111,13 +111,13 @@ def query(index):
             print(f'{value}*{things[index][value]}')
         print('-'*20)
     else:
-        print(f'Index "{index}" does not exist.')
+        print(f'索引 "{index}" 不存在。')
         log('无结果。')
 
 def display():
     global things
     if len(things) == 0:
-        print('No items.')
+        print('无物品。')
         return
     else:
         print('------------')
@@ -126,87 +126,93 @@ def display():
             for value in things[index].keys():
                 print(f'    {value}*{things[index][value]}')
             print('------------')
-        print(f'Total: {sum(sum(things[i].values()) for i in things.keys())} items in {len(things)} indexes.')
+        print(f'共 {sum(sum(things[i].values()) for i in things.keys())} 个物品在 {len(things)} 个索引中。')
 
 def login(usr:str, psw:str, num:int=3):
     log(f'用户 {usr} 想要登录。')
-    try:
-        with open(rootPath / 'users.json') as file:
-            users:dict = json.loads(file.read())
-    except FileNotFoundError:
-        log('用户数据文件不存在。')
-        log('程序退出。')
-        exit()
+    with open(rootPath / 'users.json') as file:
+        users:dict = json.loads(file.read())
     if usr in users.keys():
         if psw == users[usr]["password"]:
             log(f'用户 {usr} 登录成功。')
             global user
             user = usr
-            print('Login successful.')
-            print('Hello,', user)
+            print('登录成功。')
+            print('欢迎,', user)
             return
         else:
             if num == 0:
                 log(f'用户 {usr} 尝试3次，登录失败。')
-                print('Sorry, login failed.')
+                print('尝试太多次了！')
                 log('程序退出。')
                 exit()
             log(f'用户 {usr} 尝试 {num} 次，登录失败。')
-            print('Invalid username or password.')
-            usr = input('Username: ')
-            psw = input('Password: ')
+            print('用户名或密码错误。')
+            usr = input('用户名：')
+            psw = input('密码：')
             login(usr, psw, num-1)      
     else:
         if num == 0:
-                print('Sorry, login failed.')
+                log(f'用户 {usr} 尝试3次，登录失败。')
+                print('尝试太多次了！')
+                log('程序退出。')
                 exit()
-        print('Invalid username or password.')
-        usr = input('Username: ')
-        psw = input('Password: ')
+        print('用户名或密码错误。')
+        usr = input('用户名：')
+        psw = input('密码：')
         login(usr, psw, num-1) 
 
 def logister():
     global FILE, user
-    print('Please choose [l]ogin or [r]egister or [e]xit.')
+    print('请选择([l]登录 / [r]注册 / [e]退出)：')
     choice = input('>>> ')
     match choice.lower():
         case 'l' | 'login':
-            log('某人想要登录。')
-            usr = input('Username: ')
-            psw = input('Password: ')
+            log('开始登录。')
+            usr = input('用户名：')
+            psw = input('密码：')
             user = usr
             login(usr, psw)
             return
         case 'r' |'register':
-            log('某人想要注册。')
-            usr = input('Username: ')
-            psw = input('Password: ')
-            confirm_psw = input('Confirm Password: ')
-            if psw!= confirm_psw:
-                print('Passwords do not match.')
-                logister()
+            log('开始注册。')
+            usr = input('用户名：')
             with open(rootPath / 'users.json') as file:
                 FILE = json.load(file)
             if usr.lower() in (i.lower() for i in FILE.keys()):
-                print('Username already exists.')
+                print('用户名已存在。')
                 logister()
             else:
-                FILE[usr] = {"password": psw, "things": {}}
-                with open(rootPath / f'users.json', 'w') as file:
-                    json.dump(FILE, file, indent=4, sort_keys=True)
-                print('Registration successful.')
-                user = usr
-                login(usr, psw)
-                return
+                psw = input('密码：')
+                confirm_psw = input('确认密码：')
+                if psw!= confirm_psw:
+                    print('两次密码不匹配。')
+                    log('注册失败。')
+                    logister()
+                else:
+                    FILE[usr] = {"password": psw, "things": {}}
+                    with open(rootPath / f'users.json', 'w') as file:
+                        json.dump(FILE, file, indent=4, sort_keys=True)
+                    print('注册成功。')
+                    user = usr
+                    login(usr, psw)
+                    return
         case 'e' | 'exit':
             log('程序结束。')
             exit()
         case _:
-            print('Invalid choice.')
+            print('输入不合法。')
             logister()
 
 def main():
     initLog()
+    try:
+        with open(rootPath / 'users.json'):
+            pass
+    except FileNotFoundError:
+        log('用户数据文件不存在。')
+        log('程序退出。')
+        exit()
     print(TIPS_INFO)
     log('程序启动。')
     logister()
@@ -220,11 +226,11 @@ def main():
         match command.lower():
             case 'add' | 'a':
                 try:
-                    index = input('Index: ')
-                    value = input('Value: ')
-                    count = int(input('Count: '))
+                    index = input('索引：')
+                    value = input('物品：')
+                    count = int(input('数量：'))
                 except:
-                    print('Invalid input.')
+                    print('输入不合法。')
                     continue
                 else:
                     for _ in range(count):
@@ -232,11 +238,11 @@ def main():
 
             case 'delete' | 'dl' | 'dlt':
                 try:
-                    index = input('Index: ')
-                    value = input('Value: ')
-                    count = int(input('Count: '))
+                    index = input('索引：')
+                    value = input('物品：')
+                    count = int(input('数量：'))
                 except:
-                    print('Invalid input.')
+                    print('输入不合法。')
                     continue
                 else:
                     for _ in range(count):
@@ -244,18 +250,18 @@ def main():
 
             case'search' | 's'| 'sc' | 'find' | 'fd' | 'f':
                 try:
-                    value = input('Value: ')
+                    value = input('物品：')
                 except:
-                    print('Invalid input.')
+                    print('输入不合法。')
                     continue
                 else:
                     search(value)
 
             case 'query' | 'q':
                 try:
-                    index = input('Index: ')
+                    index = input('索引：')
                 except:
-                    print('Invalid input.')
+                    print('输入不合法。')
                     continue
                 else:
                     query(index)
@@ -268,23 +274,23 @@ def main():
 
             case 'help' | 'h':
                 print(f'''{TIPS_INFO}
-==================== Commands Guide =======================
-                    add, a - Add an item to an index.
-           delete, dl, dlt - Delete an item from an index.
-search, s, sc, find, fd, f - Search for an item.
-                  query, q - Query an index.
-                clear, cls - Clear the screen.
-           display, d, dis - Display all items.
-                   help, h - Display this help message.
-                exit, quit - Exit the program.
-===========================================================''')
+==================== 命 令 指 南 =======================
+                    add, a - 添加某物品到某索引。
+           delete, dl, dlt - 从某索引删除某物品。
+search, s, sc, find, fd, f - 查询某物品。
+                  query, q - 查询某索引。
+                clear, cls - 清屏。
+           display, d, dis - 显示所有物品。
+                   help, h - 显示这条帮助信息。
+                exit, quit - 退出程序。
+========================================================''')
 
             case 'exit' | 'quit':
                 log('程序结束。')
                 keep_going = False
 
             case _:
-                print('Invalid command.')
+                print('输入不合法。')
                 continue
 
 if __name__ == '__main__':
